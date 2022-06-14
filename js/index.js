f42298c02aeff23d3af50b4db602ad1f521d0e10
Ivan Mukoed <1rydler@gmail.com>
2022-06-14 17:30:30 +0300

    syntax fixes

--- js/index.js
+++ js/index.js
@@ -4,23 +4,21 @@ const navMenu = document.querySelector(".nav-menu");
 hamburger.addEventListener("click", mobileMenu);
 
 function mobileMenu() {
-    hamburger.classList.toggle("active");
-    navMenu.classList.toggle("active");
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
 }
 
 const navLink = document.querySelectorAll(".nav-link");
 
-navLink.forEach(n => n.addEventListener("click", closeMenu));
+navLink.forEach((n) => n.addEventListener("click", closeMenu));
 
 function closeMenu() {
-    hamburger.classList.remove("active");
-    navMenu.classList.remove("active");
+  hamburger.classList.remove("active");
+  navMenu.classList.remove("active");
 }
 
-
-
-$(document).ready(function(){
-  $('.works_carousel').slick({
+$(document).ready(function () {
+  $(".works_carousel").slick({
     slidesToShow: 5,
     autoplay: true,
     arrows: false,
@@ -32,19 +30,19 @@ $(document).ready(function(){
         settings: {
           arrows: false,
           centerMode: true,
-          centerPadding: '40px',
-          slidesToShow: 3
-        }
+          centerPadding: "40px",
+          slidesToShow: 3,
+        },
       },
       {
         breakpoint: 480,
         settings: {
           arrows: false,
           centerMode: true,
-          centerPadding: '40px',
-          slidesToShow: 1
-        }
-      }
-    ]
+          centerPadding: "40px",
+          slidesToShow: 1,
+        },
+      },
+    ],
   });
 });
